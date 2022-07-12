4021ec857c421641509fdc3e91bdf810b2ea0f18
deeptirana <deeptirana10@gmail.com>
2022-07-12 12:23:34 -0700

    countPoints

---
+++ countPoints.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def countPoints(self, rings):
+        h = defaultdict(set)
+        for i in range(0, len(rings) - 1, 2):
+            h[rings[i + 1]].add(rings[i])
+        return sum(len(v) == 3 for v in h.values())
